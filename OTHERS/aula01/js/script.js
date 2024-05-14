0b7ceb8ff55e17287b68c6d3d84381c9d5a95308
Jairo Rocha <jairoc.rocha10@gmail.com>
2024-05-14 15:26:06 -0300

    others

---
+++ OTHERS/aula01/js/script.js
@@ -0,0 +1,17 @@
+// tipos de dados
+
+console.log(10);
+console.log("jairo");
+console.log(true);
+console.log(Infinity);
+console.log(null);
+console.log(undefined);
+
+// Variaveis
+let nome = "jairo";
+let cor = "azul";
+let idade = 42;
+
+console.log(nome);
+console.log(cor);
+console.log(idade);
