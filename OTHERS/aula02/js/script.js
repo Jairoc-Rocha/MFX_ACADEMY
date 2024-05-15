642e91c7fb047473a6e457e58fc78bfbf14a7be1
Jairo Rocha <jairoc.rocha10@gmail.com>
2024-05-15 13:40:04 -0300

    others 02

---
+++ OTHERS/aula02/js/script.js
@@ -0,0 +1,17 @@
+// funções
+
+function soma(a, b) {
+  return a + b;
+}
+console.log(soma(5, 9));
+
+const subtrair = (a, b) => {
+  return a - b;
+};
+
+console.log(subtrair(45, 10));
+
+let dividir = (a, b) => {
+  return a / b;
+};
+console.log(dividir(100, 20));
