3b875dda6dd20b9a03e1b12cd24abc33f799b091
Jairo Rocha <jairoc.rocha10@gmail.com>
2024-05-24 15:04:12 -0300

    finalizando o projeto CRUD

--- FROND-END/js/testes/CRUD/js/script.js
+++ FROND-END/js/testes/CRUD/js/script.js
@@ -1,13 +1,16 @@
 window.addEventListener("load", start);
 
-let globalNames = ["Um", "Dois", "Três", "Quatro"];
+let globalNames = ["Um", "Dois", "Três", "Quatro", "Cinco"];
 let inputName = null;
+let currentIndex = null;
+let isEditing = false;
 
 function start() {
   inputName = document.querySelector("#inputName");
 
   preventFormSubmit();
   activateInput();
+  render();
 }
 
 function preventFormSubmit() {
@@ -20,5 +23,90 @@ function preventFormSubmit() {
 }
 
 function activateInput() {
+  function insertName(newName) {
+    globalNames.push(newName);
+    render();
+  }
+
+  function updateName(newName) {
+    globalNames[currentIndex] = newName;
+  }
+
+  function handleTyping(event) {
+    let hasText = !!event.target.value && event.target.value.trim() !== "";
+
+    if (!hasText) {
+      clearInput();
+      return;
+    }
+
+    if (event.key === "Enter") {
+      if (isEditing) {
+        updateName(event.target.value);
+      } else {
+        insertName(event.target.value);
+      }
+      render();
+      isEditing = false;
+      clearInput();
+    }
+  }
+  inputName.addEventListener("keyup", handleTyping);
+  inputName.focus();
+}
+
+function render() {
+  function createDeleteButton(index) {
+    function deleteName() {
+      globalNames.splice(index, 1);
+      render();
+    }
+
+    let button = document.createElement("button");
+    button.classList.add("deleteButton");
+    button.textContent = "x";
+    button.addEventListener("click", deleteName);
+    return button;
+  }
+
+  function createSpan(name, index) {
+    function editItem() {
+      inputName.value = name;
+      inputName.focus();
+      isEditing = true;
+      currentIndex = index;
+    }
+
+    let span = document.createElement("span");
+    span.classList.add("clickable");
+    span.textContent = name;
+    span.addEventListener("click", editItem);
+    return span;
+  }
+
+  let divNames = document.querySelector("#names");
+  divNames.innerHTML = "";
+
+  let ul = document.createElement("ul");
+
+  for (let i = 0; i < globalNames.length; i++) {
+    let currentName = globalNames[i];
+
+    let li = document.createElement("li");
+    let button = createDeleteButton(i);
+    let span = createSpan(currentName, i);
+
+    li.appendChild(button);
+    li.appendChild(span);
+
+    ul.appendChild(li);
+  }
+
+  divNames.appendChild(ul);
+  clearInput();
+}
+
+function clearInput() {
+  inputName.value = "";
   inputName.focus();
 }
