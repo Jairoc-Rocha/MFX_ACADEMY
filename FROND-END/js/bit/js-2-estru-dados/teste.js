59cc362eebe5345f91b9568c074df0556700081e
Jairo Rocha <jairoc.rocha10@gmail.com>
2024-08-07 20:57:01 -0300

    commit 4

---
+++ FROND-END/js/bit/js-2-estru-dados/teste.js
@@ -0,0 +1,10 @@
+const pessoa = {
+  nome: "Jairo",
+  idade: 42,
+  profissao: "Programador",
+};
+
+console.log(pessoa);
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(pessoa.profissao);
